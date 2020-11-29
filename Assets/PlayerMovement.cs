abce09ad7e597838a1dffc54ea0a8e9ce5c168bb
C.Rondeau <rondeau.clement@gmail.com>
2020-11-29 22:48:02 +0100

    + implement basic jump

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -7,8 +7,10 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private Rigidbody2D m_rb;
     [SerializeField] private float m_speed;
+    [SerializeField] private float m_jumpHeight;
 
     private float m_forward = 0f;
+    private float m_jump = 0f;
 
     private void Start()
     {
@@ -25,6 +27,11 @@ public class PlayerMovement : MonoBehaviour
         {
             m_forward = -1f;
         }
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            m_jump = 1f;
+        }
     }
 
     private void FixedUpdate()
@@ -38,6 +45,15 @@ public class PlayerMovement : MonoBehaviour
             m_rb.velocity = new Vector2(0f, m_rb.velocity.y);
         }
 
+        if (m_jump != 0f)
+        {
+            m_rb.AddForce(new Vector2(0f, m_jumpHeight), ForceMode2D.Impulse);
+        }
+
         m_forward = 0f;
+        m_jump = 0f;
+
+        Physics2D.Raycast(transform.position, transform.up * -1f, 10f);
+        //TODO: Upgrade with layer of enviro
     }
 }
