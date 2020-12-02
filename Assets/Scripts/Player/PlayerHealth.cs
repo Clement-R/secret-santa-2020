fe967448821a5b0177370e8976e7a0e5bd19b313
C.Rondeau <rondeau.clement@gmail.com>
2020-12-02 09:39:56 +0100

    + add health, traps and detection

---
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    [SerializeField] private string m_trapLayerName = "Trap";
+    [SerializeField] private float m_invincibilityDuration = 0.2f;
+
+    private float m_lastDamageTaken = 0f;
+
+    private void TakeDamage()
+    {
+        if (Time.time <= m_lastDamageTaken + m_invincibilityDuration)
+        {
+            return;
+        }
+
+        m_lastDamageTaken = Time.time;
+    }
+
+    private void OnTriggerEnter2D(Collider2D p_other)
+    {
+        if (IsATrap(p_other))
+        {
+            TakeDamage();
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D p_other)
+    {
+        if (IsATrap(p_other))
+        {
+            TakeDamage();
+        }
+    }
+
+    private bool IsATrap(Collider2D p_collider)
+    {
+        return p_collider.gameObject.layer == LayerMask.NameToLayer(m_trapLayerName);
+    }
+}
