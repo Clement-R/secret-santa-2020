94c251eb4680d28280504093c1e36acc668722ff
C.Rondeau <rondeau.clement@gmail.com>
2020-12-02 23:06:12 +0100

    _ tidy up project and add checkpoint animation and FX logic

---
+++ Assets/CheckpointBehaviour.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+public class CheckpointBehaviour : MonoBehaviour
+{
+    [SerializeField] private string m_playerLayerName;
+    [SerializeField] private Animator m_animator;
+    [SerializeField] private string m_animatorOnParameter = "On";
+
+    private void OnTriggerEnter2D(Collider2D p_collider)
+    {
+        if (p_collider.gameObject.layer == LayerMask.NameToLayer(m_playerLayerName))
+        {
+            m_animator.SetBool(m_animatorOnParameter, true);
+        }
+    }
+}
