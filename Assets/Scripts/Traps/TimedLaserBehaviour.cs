669ce7a882301fe58f432864c0cec65fe4a2dc05
C.Rondeau <rondeau.clement@gmail.com>
2020-12-08 09:23:08 +0100

    + add sounds for timed laser, but it's not great

--- Assets/Scripts/Traps/TimedLaserBehaviour.cs
+++ Assets/Scripts/Traps/TimedLaserBehaviour.cs
@@ -8,7 +8,13 @@ public class TimedLaserBehaviour : ActivationTrap
     [SerializeField] private float m_delayDuration;
     [SerializeField] private float m_offsetDuration;
     [SerializeField] private float m_activeDuration;
-    [SerializeField] private GameObject m_laser;
+    [SerializeField] private float m_previewDuration;
+    [SerializeField] private SpriteRenderer m_laser;
+    [SerializeField] private Sprite m_laserSprite;
+    [SerializeField] private Sprite m_laserPreviewSprite;
+    [SerializeField] private SFX m_laserPreviewSFX;
+    [SerializeField] private SFX m_laserActivateSFX;
+    [SerializeField] private Collider2D m_collider;
 
     private float m_lastActivation = 0f;
     private float m_nextActivation = 0f;
@@ -23,6 +29,11 @@ public class TimedLaserBehaviour : ActivationTrap
 
     void Update()
     {
+        if (Time.time > m_nextActivation - m_previewDuration)
+        {
+            PreActivate();
+        }
+
         if (Time.time > m_nextActivation)
         {
             Activate();
@@ -34,15 +45,27 @@ public class TimedLaserBehaviour : ActivationTrap
         }
     }
 
+    private void PreActivate()
+    {
+        m_laser.sprite = m_laserPreviewSprite;
+
+        // SoundsManager.Instance.PlayOneShot(m_laserPreviewSFX);
+    }
+
     protected override void Activate(bool p_silent = false)
     {
-        m_laser.SetActive(true);
+        m_collider.enabled = true;
+
+        m_laser.sprite = m_laserSprite;
         m_lastActivation = Time.time;
         m_nextActivation = m_lastActivation + m_activeDuration + m_delayDuration;
+
+        // SoundsManager.Instance.PlayOneShot(m_laserActivateSFX);
     }
 
     protected override void Deactivate(bool p_silent = false)
     {
-        m_laser.SetActive(false);
+        m_collider.enabled = false;
+        m_laser.sprite = null;
     }
 }
